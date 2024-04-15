a3f2d7513588942705ec0f87e2d69346f6ad0570
Sergi Majada <118728080+smajada@users.noreply.github.com>
2024-04-15 10:17:18 +0200

    feat: implemented crear_regla page

--- sersoc-api/src/main/java/org/arteco/sersoc/controller/ReglaTipoPrestacionController.java
+++ sersoc-api/src/main/java/org/arteco/sersoc/controller/ReglaTipoPrestacionController.java
@@ -3,14 +3,17 @@ package org.arteco.sersoc.controller;
 import org.arteco.sersoc.base.AbstractCrudController;
 import org.arteco.sersoc.model.base.ReglasTipoPrestacionId;
 import org.arteco.sersoc.model.entities.ReglaTipoPrestacionEntity;
+import org.arteco.sersoc.model.entities.TipoPrestacionEntity;
 import org.arteco.sersoc.repository.ReglaTipoPrestacionRepository;
 import org.arteco.sersoc.service.ReglaTipoPrestacionService;
+import org.arteco.sersoc.service.TipoPrestacionService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -23,8 +26,11 @@ import java.util.stream.IntStream;
 @RequestMapping("/regla-tipo-prestacion")
 public class ReglaTipoPrestacionController extends AbstractCrudController<ReglaTipoPrestacionEntity, ReglasTipoPrestacionId, ReglaTipoPrestacionRepository, ReglaTipoPrestacionService> {
 
-    protected ReglaTipoPrestacionController(ReglaTipoPrestacionService service) {
+    private final TipoPrestacionService tipoPrestacionService;
+
+    protected ReglaTipoPrestacionController(ReglaTipoPrestacionService service, TipoPrestacionService tipoPrestacionService) {
         super(service);
+        this.tipoPrestacionService = tipoPrestacionService;
     }
 
     @GetMapping("/list")
@@ -40,8 +46,6 @@ public class ReglaTipoPrestacionController extends AbstractCrudController<ReglaT
 
         // Se obtiene la lista de reglas de tipo prestación paginada y filtrada por nombre
         Page<ReglaTipoPrestacionEntity> reglasTipoPrestacionPage = service.findPaginated(PageRequest.of(currentPage - 1, pageSize), keyword);
-
-
 
         int totalPages = reglasTipoPrestacionPage.getTotalPages();
         // Si el total de páginas es mayor a 0, se crea una lista con los números de las páginas
@@ -62,9 +66,26 @@ public class ReglaTipoPrestacionController extends AbstractCrudController<ReglaT
     public String crearReglasTipoPrestacion(Model model){
         ReglaTipoPrestacionEntity reglaTipoPrestacion = new ReglaTipoPrestacionEntity();
         model.addAttribute("reglaTipoPrestacion", reglaTipoPrestacion);
+        List<TipoPrestacionEntity> tipoPrestacionList = (List<TipoPrestacionEntity>) tipoPrestacionService.findAll();
+        model.addAttribute("tipoPrestacionList", tipoPrestacionList);
+
         model.addAttribute("titlePage", "Nueva regla tipo prestacion");
         return "reglas/crear_regla";
     }
+
+//    @GetMapping("/editar/{reglaId}/{tipoPrestacionId}")
+//    public String editarReglasTipoPrestacion(Model model, @PathVariable(value = "reglaId") Long reglaId, @PathVariable(value = "tipoPrestacionId") Long tipoPrestacionId){
+//        ReglasTipoPrestacionId id = new ReglasTipoPrestacionId();
+//        id.setReglaId(reglaId);
+//        id.setTipoPrestacionId(tipoPrestacionId);
+//
+//        ReglaTipoPrestacionEntity reglaTipoPrestacion = service.findById(id).get();
+//        List<TipoPrestacionEntity> tipoPrestacionList = (List<TipoPrestacionEntity>) tipoPrestacionService.findAll();
+//        model.addAttribute("reglaTipoPrestacion", reglaTipoPrestacion);
+//        model.addAttribute("tipoPrestacionList", tipoPrestacionList);
+//        model.addAttribute("titlePage", "Editar regla tipo prestacion");
+//        return "reglas/crear_regla";
+//    }
 
 
 }
