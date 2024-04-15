bdb86d49a26e9a3fe7918f213f8af17688070727
Sergi Majada <118728080+smajada@users.noreply.github.com>
2024-04-15 13:54:54 +0200

    feat: added PageDTO

---
+++ sersoc-api/src/main/java/org/arteco/sersoc/dto/PageDto.java
@@ -0,0 +1,39 @@
+package org.arteco.sersoc.dto;
+
+import com.fasterxml.jackson.annotation.JsonView;
+import lombok.Data;
+import org.springframework.data.domain.Page;
+
+import javax.swing.text.html.ListView;
+import java.util.List;
+
+@Data
+public class PageDto<ENTITY> {
+
+    @JsonView(ListView.class)
+    private final int totalPages;
+
+    @JsonView(ListView.class)
+    private final long totalElements;
+
+    @JsonView(ListView.class)
+    private final List<ENTITY> content;
+
+    @JsonView(ListView.class)
+    private final int size;
+
+    @JsonView(ListView.class)
+    private final int numberOfElements;
+
+    @JsonView(ListView.class)
+    private final int number;
+
+    public PageDto(Page<ENTITY> page) {
+        this.totalPages = page.getTotalPages();
+        this.totalElements = page.getTotalElements();
+        this.content = page.getContent();
+        this.number = page.getNumber();
+        this.numberOfElements = page.getNumberOfElements();
+        this.size = page.getSize();
+    }
+}
