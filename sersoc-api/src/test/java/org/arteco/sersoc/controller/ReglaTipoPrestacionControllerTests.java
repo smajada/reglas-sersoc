27098651a43cf83b7aef7eac26d4b9924d5171d2
Sergi Majada <118728080+smajada@users.noreply.github.com>
2024-04-19 13:48:04 +0200

    test: created tests for controller and service

---
+++ sersoc-api/src/test/java/org/arteco/sersoc/controller/ReglaTipoPrestacionControllerTests.java
@@ -0,0 +1,173 @@
+package org.arteco.sersoc.controller;
+
+
+import org.arteco.sersoc.dto.PageDto;
+import org.arteco.sersoc.model.entities.NoutRegles;
+import org.arteco.sersoc.model.entities.NoutTipprs;
+import org.arteco.sersoc.model.entities.ReglaTipoPrestacionEntity;
+import org.arteco.sersoc.service.NoutReglesService;
+import org.arteco.sersoc.service.NoutTipprsService;
+import org.arteco.sersoc.service.ReglaTipoPrestacionService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.ui.Model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.*;
+
+
+@WebMvcTest(ReglaTipoPrestacionController.class)
+public class ReglaTipoPrestacionControllerTests {
+
+    @MockBean
+    private NoutTipprsService noutTipprsService;
+
+    @MockBean
+    private NoutReglesService noutReglesService;
+
+    @MockBean
+    private NoutReglesController noutReglesController;
+
+    @MockBean
+    private ReglaTipoPrestacionService service;
+
+    @MockBean
+    private Model model;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+
+    @Test
+    void ReglasTipoPrestacionController_listAll_ReturnRedirect() throws Exception {
+        Pageable pageRequest = PageRequest.of(0, 20);
+
+        List<NoutRegles> regles = new ArrayList<>();
+        List<ReglaTipoPrestacionEntity> reglasTipoPrestacion = new ArrayList<>();
+
+        PageDto<NoutRegles> reglesPage = new PageDto<>(new PageImpl<>(regles));
+        PageDto<ReglaTipoPrestacionEntity> reglasTipoPrestacionPage = new PageDto<>(new PageImpl<>(reglasTipoPrestacion));
+
+        when(service.page(pageRequest)).thenReturn(new PageImpl<>(reglasTipoPrestacion));
+        when(noutReglesController.page(pageRequest)).thenReturn(reglesPage);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/regla-tipo-prestacion/list"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("reglas/reglas"))
+                .andExpect(MockMvcResultMatchers.model().attribute("totalPages", reglesPage.getTotalPages()))
+                .andExpect(MockMvcResultMatchers.model().attribute("reglasTipoPrestacion", reglasTipoPrestacionPage.getContent()))
+                .andExpect(MockMvcResultMatchers.model().attribute("reglas", reglesPage.getContent()))
+                .andExpect(MockMvcResultMatchers.model().attribute("currentPage", 0))
+                .andExpect(MockMvcResultMatchers.model().attribute("titlePage", "Reglas "));
+
+    }
+
+    @Test
+    void shouldShowEditarReglasPage() throws Exception {
+        Long reglaId = 1L;
+
+        NoutRegles regla = new NoutRegles();
+        regla.setCon(1L);
+
+        List<NoutTipprs> allTipoPrestacion = new ArrayList<>();
+        List<ReglaTipoPrestacionEntity> allReglasTipoPrestacion = new ArrayList<>();
+
+        ReglaTipoPrestacionEntity reglaTipoPrestacion = new ReglaTipoPrestacionEntity();
+
+        when(noutReglesService.findById(reglaId)).thenReturn(java.util.Optional.of(regla));
+        when(noutTipprsService.findAll()).thenReturn(allTipoPrestacion);
+        when(service.findAll()).thenReturn(allReglasTipoPrestacion);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/regla-tipo-prestacion/editar/1"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("reglas/editar_regla"))
+                .andExpect(MockMvcResultMatchers.model().attribute("reglaTipoPrestacion", reglaTipoPrestacion))
+                .andExpect(MockMvcResultMatchers.model().attribute("regla", regla))
+                .andExpect(MockMvcResultMatchers.model().attribute("allTipoPrestacion", allTipoPrestacion))
+                .andExpect(MockMvcResultMatchers.model().attribute("reglasTipoPrestacionId", new ArrayList<>()))
+                .andExpect(MockMvcResultMatchers.model().attribute("titlePage", "Editar regla")
+                );
+
+    }
+
+    @Test
+    void shouldDeleteRegla() throws Exception {
+        Long reglaId = 1L;
+
+        NoutRegles regla = new NoutRegles();
+        regla.setCon(reglaId);
+
+        when(noutReglesService.findById(reglaId)).thenReturn(java.util.Optional.of(regla));
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/regla-tipo-prestacion/delete/1"))
+                .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
+                .andExpect(MockMvcResultMatchers.view().name("redirect:/regla-tipo-prestacion/list"));
+
+        verify(noutReglesService, times(1)).delete(regla);
+    }
+
+    @Test
+    void shouldShowCrearReglasPage() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/regla-tipo-prestacion/crear"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("reglas/crear_regla"))
+                .andExpect(MockMvcResultMatchers.model().attribute("regla", new NoutRegles()))
+                .andExpect(MockMvcResultMatchers.model().attribute("tipoPrestaciones", noutTipprsService.findAll()));
+    }
+
+    @Test
+    void shouldRedirectFromSave() throws Exception {
+        NoutRegles regla = new NoutRegles();
+        regla.setCon(1L);
+
+        // Simula la lista de tipo de prestaciones devuelta por el servicio
+        List<NoutTipprs> tipoPrestaciones = new ArrayList<>();
+        tipoPrestaciones.add(new NoutTipprs());
+
+        // Configura el comportamiento esperado del servicio
+        when(noutTipprsService.findAllById(anyList())).thenReturn(tipoPrestaciones);
+
+        // Realiza la solicitud POST con los parámetros correctos
+        mockMvc.perform(MockMvcRequestBuilders.post("/regla-tipo-prestacion/save")
+                        .flashAttr("regla", regla)
+                        .param("tipoPrestacion", String.valueOf(tipoPrestaciones)))
+                .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
+                .andExpect(MockMvcResultMatchers.view().name("redirect:/regla-tipo-prestacion/list"));
+
+    }
+
+    @Test
+    void shouldRedirectFromUpdate() throws Exception {
+        Long reglaId = 1L;
+        NoutRegles regla = new NoutRegles();
+        regla.setCon(reglaId);
+
+        // Simula la lista de tipo de prestaciones devuelta por el servicio
+        List<NoutTipprs> tipoPrestaciones = new ArrayList<>();
+        tipoPrestaciones.add(new NoutTipprs());
+
+        // Configura el comportamiento esperado del servicio
+        when(noutTipprsService.findAllById(anyList())).thenReturn(tipoPrestaciones);
+
+        // Realiza la solicitud POST con los parámetros correctos
+        mockMvc.perform(MockMvcRequestBuilders.post("/regla-tipo-prestacion/save/1")
+                        .flashAttr("regla", regla)
+                        .param("tipoPrestacion", String.valueOf(tipoPrestaciones)))
+                .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
+                .andExpect(MockMvcResultMatchers.view().name("redirect:/regla-tipo-prestacion/list"));
+
+    }
+
+}
---
+++ sersoc-api/src/test/java/org/arteco/sersoc/service/ReglaTipoPrestacionServiceTest.java
@@ -0,0 +1,81 @@
+package org.arteco.sersoc.service;
+
+import org.arteco.sersoc.model.entities.NoutRegles;
+import org.arteco.sersoc.model.entities.NoutTipprs;
+import org.arteco.sersoc.model.entities.ReglaTipoPrestacionEntity;
+import org.arteco.sersoc.repository.NoutReglesRepository;
+import org.arteco.sersoc.repository.ReglaTipoPrestacionRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class ReglaTipoPrestacionServiceTest {
+
+    @Mock
+    private ReglaTipoPrestacionRepository reglaTipoPrestacionRepository;
+
+    @Mock
+    private NoutReglesRepository noutReglesRepository;
+
+    @InjectMocks
+    private ReglaTipoPrestacionService reglaTipoPrestacionService;
+
+    @Test
+    public void ReglaTipoPrestacionService_save_ReturnRTP(){
+        NoutRegles regla = new NoutRegles();
+        List<NoutTipprs> tiposPrestacion = new ArrayList<>();
+
+        for (int i = 0; i < 5; i++) {
+            NoutTipprs tipoPrestacion = new NoutTipprs();
+            tipoPrestacion.setCoa(String.valueOf(i));
+            tiposPrestacion.add(tipoPrestacion);
+        }
+
+        when(noutReglesRepository.save(any())).thenReturn(regla);
+
+        reglaTipoPrestacionService.save(regla, tiposPrestacion);
+
+        verify(noutReglesRepository, times(1)).save(any());
+        verify(reglaTipoPrestacionRepository, times(5)).save(any());
+    }
+
+    @Test
+    public void ReglaTipoPrestacionService_updateAll_ReturnRTP(){
+        NoutRegles regla = new NoutRegles();
+        List<NoutTipprs> tiposPrestacion = new ArrayList<>();
+
+        for (int i = 0; i < 5; i++) {
+            NoutTipprs tipoPrestacion = new NoutTipprs();
+            tipoPrestacion.setCoa(String.valueOf(i));
+            tiposPrestacion.add(tipoPrestacion);
+        }
+
+        when(noutReglesRepository.save(any())).thenReturn(regla);
+
+        reglaTipoPrestacionService.updateAll(1L, regla, tiposPrestacion);
+
+        verify(noutReglesRepository, times(1)).save(any());
+        verify(reglaTipoPrestacionRepository, times(5)).save(any());
+    }
+
+    @Test
+    public void ReglaTipoPrestacionService_delete_ReturnFalse(){
+        ReglaTipoPrestacionEntity reglaTipoPrestacion = new ReglaTipoPrestacionEntity();
+
+        reglaTipoPrestacionService.delete(reglaTipoPrestacion);
+
+        assertFalse(reglaTipoPrestacion.getActive());
+    }
+
+
+}
