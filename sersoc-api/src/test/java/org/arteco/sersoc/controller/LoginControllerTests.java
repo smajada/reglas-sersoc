0435696896bb2eef96f92b9a723a7a887b5bc321
Sergi Majada <118728080+smajada@users.noreply.github.com>
2024-05-24 13:38:30 +0200

    test: tested LoginController

--- sersoc-api/src/test/java/org/arteco/sersoc/controller/LoginControllerTests.java
+++ sersoc-api/src/test/java/org/arteco/sersoc/controller/LoginControllerTests.java
@@ -1,55 +1,57 @@
 package org.arteco.sersoc.controller;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.mockito.Mockito.verify;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
 
-@WebMvcTest(LoginController.class)
 public class LoginControllerTests {
 
-	@Autowired
-	private MockMvc mockMvc;
+	@InjectMocks
+	private LoginController loginController;
 
-	@Test
-	@WithMockUser(username = "admin", roles = {"ADMIN"})
-	void LoginController_publicPage_ReturnRedirect() throws Exception {
+	@Mock
+	private Model model;
 
-		mockMvc.perform(get("/public"))
-			.andExpect(status().isOk())
-			.andExpect(view().name("autenticate/prueba"));
+	@BeforeEach
+	public void setUp() {
+
+		MockitoAnnotations.initMocks(this);
 	}
 
 	@Test
-	@WithMockUser(username = "admin", roles = {"ADMIN"})
-	void loginController_authenticatedUser_ReturnRedirect() throws Exception {
+	public void testLogin() {
 
-		// Perform the request and capture the result
-		MvcResult result = mockMvc.perform(get("/login"))
-			.andExpect(status().is3xxRedirection())
-			.andExpect(redirectedUrl("/home"))
-			.andReturn();
-
-		// Log the response for debugging purposes
-		System.out.println("Response: " + result.getResponse().getContentAsString());
+		String viewName = loginController.login(model);
+		assert viewName.equals("autenticate/login");
+		verify(model).addAttribute("titlePage", "Login");
 	}
 
 	@Test
-	void loginController_unauthenticatedUser_ReturnLoginView() throws Exception {
+	public void testIndex() {
 
-		mockMvc.perform(get("/login"))
-			.andExpect(status().isOk())
-			.andExpect(view().name("autenticate/login"))
-			.andExpect(model().attributeExists("titlePage"))
-			.andExpect(model().attribute("titlePage", "Login"))
-			.andExpect(model().attributeExists("username"));
+		String viewName = loginController.index(model);
+		assert viewName.equals("autenticate/home");
+		verify(model).addAttribute("titlePage", "Home");
+	}
+
+	@Test
+	public void testAccessDenied() {
+
+		String viewName = loginController.accessDenied(model);
+		assert viewName.equals("error/access-denied");
+		verify(model).addAttribute("titlePage", "Access Denied");
+	}
+
+	@Test
+	public void testNotFound() {
+
+		String viewName = loginController.notFound(model);
+		assert viewName.equals("error/not-found");
+		verify(model).addAttribute("titlePage", "Not-found");
 	}
 }
