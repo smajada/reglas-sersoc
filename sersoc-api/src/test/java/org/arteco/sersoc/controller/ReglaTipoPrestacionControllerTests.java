40eac8af39c92821f07988c5fe95ef7bca2be2d2
Sergi Majada <118728080+smajada@users.noreply.github.com>
2024-05-27 11:51:17 +0200

    WIP: RTPController Test

--- sersoc-api/src/test/java/org/arteco/sersoc/controller/ReglaTipoPrestacionControllerTests.java
+++ sersoc-api/src/test/java/org/arteco/sersoc/controller/ReglaTipoPrestacionControllerTests.java
@@ -1,10 +1,19 @@
 package org.arteco.sersoc.controller;
 
 
-import org.arteco.sersoc.dto.PageDTO;
+import static org.mockito.Mockito.anyList;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import org.arteco.sersoc.model.entities.NoutRegles;
 import org.arteco.sersoc.model.entities.NoutTipprs;
 import org.arteco.sersoc.model.entities.ReglaTipoPrestacionEntity;
+import org.arteco.sersoc.service.NashornService;
+import org.arteco.sersoc.service.NoutPrestacionsService;
 import org.arteco.sersoc.service.NoutReglesService;
 import org.arteco.sersoc.service.NoutTipprsService;
 import org.arteco.sersoc.service.ReglaTipoPrestacionService;
@@ -12,157 +21,159 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.mockito.Mockito.*;
 
 
 @WebMvcTest(ReglaTipoPrestacionController.class)
 public class ReglaTipoPrestacionControllerTests {
 
-    @MockBean
-    private NoutTipprsService noutTipprsService;
+	@Autowired
+	MockMvc mockMvc;
 
-    @MockBean
-    private NoutReglesService noutReglesService;
+	@MockBean
+	private NoutTipprsService noutTipprsService;
 
-    @MockBean
-    private NoutReglesController noutReglesController;
+	@MockBean
+	private NoutReglesService noutReglesService;
 
-    @MockBean
-    private ReglaTipoPrestacionService service;
+	@MockBean
+	private ReglaTipoPrestacionService reglaTipoPrestacionService;
+
+	@MockBean
+	private NoutPrestacionsService noutPrestacionsService;
+
+	@MockBean
+	private NashornService nashornService;
 
 
-    @Autowired
-    private MockMvc mockMvc;
+	@Test
+	@WithMockUser(username = "admin", roles = {"ADMIN"})
+	void ReglasTipoPrestacionController_listAll_ReturnRedirect() throws Exception {
+
+		Pageable pageRequest = PageRequest.of(0, 20);
+		Iterable<ReglaTipoPrestacionEntity> reglasTipoPrestacionPage = new PageImpl<>(new ArrayList<>(), pageRequest, 0);
+		Page<NoutRegles> reglas = new PageImpl<>(new ArrayList<>(), pageRequest, 0);
+
+		//		when(noutReglesService.findAll()).thenReturn(reglasTipoPrestacionPage);
+		when(noutReglesService.findByActiveTrue(pageRequest)).thenReturn(reglas);
+
+		mockMvc.perform(MockMvcRequestBuilders.get("/regla-tipo-prestacion/list"))
+			.andExpect(MockMvcResultMatchers.status().isOk())
+			.andExpect(MockMvcResultMatchers.view().name("reglas/reglas"))
+			.andExpect(MockMvcResultMatchers.model().attributeExists("reglas"))
+			.andExpect(MockMvcResultMatchers.model().attributeExists("totalPages"))
+			.andExpect(MockMvcResultMatchers.model().attributeExists("reglasTipoPrestacion"))
+			.andExpect(MockMvcResultMatchers.model().attributeExists("currentPage"))
+			.andExpect(MockMvcResultMatchers.model().attribute("titlePage", "Reglas "));
+	}
 
 
-    @Test
-    void ReglasTipoPrestacionController_listAll_ReturnRedirect() throws Exception {
-        Pageable pageRequest = PageRequest.of(0, 20);
+	@Test
+	void shouldShowEditarReglasPage() throws Exception {
 
-        List<NoutRegles> regles = new ArrayList<>();
-        List<ReglaTipoPrestacionEntity> reglasTipoPrestacion = new ArrayList<>();
+		Long reglaId = 1L;
 
-        PageDTO<NoutRegles> reglesPage = new PageDTO<>(new PageImpl<>(regles));
-        PageDTO<ReglaTipoPrestacionEntity> reglasTipoPrestacionPage = new PageDTO<>(new PageImpl<>(reglasTipoPrestacion));
+		NoutRegles regla = new NoutRegles();
+		regla.setCon(1L);
 
-        when(service.page(pageRequest)).thenReturn(new PageImpl<>(reglasTipoPrestacion));
-        when(noutReglesController.page(pageRequest)).thenReturn(reglesPage);
+		List<NoutTipprs> allTipoPrestacion = new ArrayList<>();
+		List<ReglaTipoPrestacionEntity> allReglasTipoPrestacion = new ArrayList<>();
 
-        mockMvc.perform(MockMvcRequestBuilders.get("/regla-tipo-prestacion/list"))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.view().name("reglas/reglas"))
-                .andExpect(MockMvcResultMatchers.model().attribute("totalPages", reglesPage.getTotalPages()))
-                .andExpect(MockMvcResultMatchers.model().attribute("reglasTipoPrestacion", reglasTipoPrestacionPage.getContent()))
-                .andExpect(MockMvcResultMatchers.model().attribute("reglas", reglesPage.getContent()))
-                .andExpect(MockMvcResultMatchers.model().attribute("currentPage", 0))
-                .andExpect(MockMvcResultMatchers.model().attribute("titlePage", "Reglas "));
+		ReglaTipoPrestacionEntity reglaTipoPrestacion = new ReglaTipoPrestacionEntity();
 
-    }
+		//		when(service.findById(reglaId)).thenReturn(Optional.of(regla));
+		when(noutTipprsService.findAll()).thenReturn(allTipoPrestacion);
+		//		when(service.findAll()).thenReturn(allReglasTipoPrestacion);
 
-    @Test
-    void shouldShowEditarReglasPage() throws Exception {
-        Long reglaId = 1L;
+		mockMvc.perform(MockMvcRequestBuilders.get("/regla-tipo-prestacion/editar/1"))
+			.andExpect(MockMvcResultMatchers.status().isOk())
+			.andExpect(MockMvcResultMatchers.view().name("reglas/editar_regla"))
+			.andExpect(MockMvcResultMatchers.model().attribute("reglaTipoPrestacion", reglaTipoPrestacion))
+			.andExpect(MockMvcResultMatchers.model().attribute("regla", regla))
+			.andExpect(MockMvcResultMatchers.model().attribute("allTipoPrestacion", allTipoPrestacion))
+			.andExpect(MockMvcResultMatchers.model().attribute("reglasTipoPrestacionId", new ArrayList<>()))
+			.andExpect(MockMvcResultMatchers.model().attribute("titlePage", "Editar regla")
+			);
 
-        NoutRegles regla = new NoutRegles();
-        regla.setCon(1L);
+	}
 
-        List<NoutTipprs> allTipoPrestacion = new ArrayList<>();
-        List<ReglaTipoPrestacionEntity> allReglasTipoPrestacion = new ArrayList<>();
+	@Test
+	void shouldDeleteRegla() throws Exception {
 
-        ReglaTipoPrestacionEntity reglaTipoPrestacion = new ReglaTipoPrestacionEntity();
+		Long reglaId = 1L;
 
-        when(noutReglesService.findById(reglaId)).thenReturn(java.util.Optional.of(regla));
-        when(noutTipprsService.findAll()).thenReturn(allTipoPrestacion);
-        when(service.findAll()).thenReturn(allReglasTipoPrestacion);
+		NoutRegles regla = new NoutRegles();
+		regla.setCon(reglaId);
 
-        mockMvc.perform(MockMvcRequestBuilders.get("/regla-tipo-prestacion/editar/1"))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.view().name("reglas/editar_regla"))
-                .andExpect(MockMvcResultMatchers.model().attribute("reglaTipoPrestacion", reglaTipoPrestacion))
-                .andExpect(MockMvcResultMatchers.model().attribute("regla", regla))
-                .andExpect(MockMvcResultMatchers.model().attribute("allTipoPrestacion", allTipoPrestacion))
-                .andExpect(MockMvcResultMatchers.model().attribute("reglasTipoPrestacionId", new ArrayList<>()))
-                .andExpect(MockMvcResultMatchers.model().attribute("titlePage", "Editar regla")
-                );
+		when(noutReglesService.findById(reglaId)).thenReturn(Optional.of(regla));
 
-    }
+		mockMvc.perform(MockMvcRequestBuilders.get("/regla-tipo-prestacion/delete/1"))
+			.andExpect(MockMvcResultMatchers.status().is3xxRedirection())
+			.andExpect(MockMvcResultMatchers.view().name("redirect:/regla-tipo-prestacion/list"));
 
-    @Test
-    void shouldDeleteRegla() throws Exception {
-        Long reglaId = 1L;
+		verify(noutReglesService, times(1)).delete(regla);
+	}
 
-        NoutRegles regla = new NoutRegles();
-        regla.setCon(reglaId);
+	@Test
+	void shouldShowCrearReglasPage() throws Exception {
 
-        when(noutReglesService.findById(reglaId)).thenReturn(java.util.Optional.of(regla));
+		mockMvc.perform(MockMvcRequestBuilders.get("/regla-tipo-prestacion/crear"))
+			.andExpect(MockMvcResultMatchers.status().isOk())
+			.andExpect(MockMvcResultMatchers.view().name("reglas/crear_regla"))
+			.andExpect(MockMvcResultMatchers.model().attribute("regla", new NoutRegles()))
+			.andExpect(MockMvcResultMatchers.model().attribute("tipoPrestaciones", noutTipprsService.findAll()));
+	}
 
-        mockMvc.perform(MockMvcRequestBuilders.get("/regla-tipo-prestacion/delete/1"))
-                .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
-                .andExpect(MockMvcResultMatchers.view().name("redirect:/regla-tipo-prestacion/list"));
+	@Test
+	void shouldRedirectFromSave() throws Exception {
 
-        verify(noutReglesService, times(1)).delete(regla);
-    }
+		NoutRegles regla = new NoutRegles();
+		regla.setCon(1L);
 
-    @Test
-    void shouldShowCrearReglasPage() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/regla-tipo-prestacion/crear"))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.view().name("reglas/crear_regla"))
-                .andExpect(MockMvcResultMatchers.model().attribute("regla", new NoutRegles()))
-                .andExpect(MockMvcResultMatchers.model().attribute("tipoPrestaciones", noutTipprsService.findAll()));
-    }
+		// Simula la lista de tipo de prestaciones devuelta por el servicio
+		List<NoutTipprs> tipoPrestaciones = new ArrayList<>();
+		tipoPrestaciones.add(new NoutTipprs());
 
-    @Test
-    void shouldRedirectFromSave() throws Exception {
-        NoutRegles regla = new NoutRegles();
-        regla.setCon(1L);
+		// Configura el comportamiento esperado del servicio
+		when(noutTipprsService.findAllById(anyList())).thenReturn(tipoPrestaciones);
 
-        // Simula la lista de tipo de prestaciones devuelta por el servicio
-        List<NoutTipprs> tipoPrestaciones = new ArrayList<>();
-        tipoPrestaciones.add(new NoutTipprs());
+		// Realiza la solicitud POST con los parámetros correctos
+		mockMvc.perform(MockMvcRequestBuilders.post("/regla-tipo-prestacion/save")
+							.flashAttr("regla", regla)
+							.param("tipoPrestacion", String.valueOf(tipoPrestaciones)))
+			.andExpect(MockMvcResultMatchers.status().is3xxRedirection())
+			.andExpect(MockMvcResultMatchers.view().name("redirect:/regla-tipo-prestacion/list"));
 
-        // Configura el comportamiento esperado del servicio
-        when(noutTipprsService.findAllById(anyList())).thenReturn(tipoPrestaciones);
+	}
 
-        // Realiza la solicitud POST con los parámetros correctos
-        mockMvc.perform(MockMvcRequestBuilders.post("/regla-tipo-prestacion/save")
-                        .flashAttr("regla", regla)
-                        .param("tipoPrestacion", String.valueOf(tipoPrestaciones)))
-                .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
-                .andExpect(MockMvcResultMatchers.view().name("redirect:/regla-tipo-prestacion/list"));
+	@Test
+	void shouldRedirectFromUpdate() throws Exception {
 
-    }
+		Long reglaId = 1L;
+		NoutRegles regla = new NoutRegles();
+		regla.setCon(reglaId);
 
-    @Test
-    void shouldRedirectFromUpdate() throws Exception {
-        Long reglaId = 1L;
-        NoutRegles regla = new NoutRegles();
-        regla.setCon(reglaId);
+		// Simula la lista de tipo de prestaciones devuelta por el servicio
+		List<NoutTipprs> tipoPrestaciones = new ArrayList<>();
+		tipoPrestaciones.add(new NoutTipprs());
 
-        // Simula la lista de tipo de prestaciones devuelta por el servicio
-        List<NoutTipprs> tipoPrestaciones = new ArrayList<>();
-        tipoPrestaciones.add(new NoutTipprs());
+		// Configura el comportamiento esperado del servicio
+		when(noutTipprsService.findAllById(anyList())).thenReturn(tipoPrestaciones);
 
-        // Configura el comportamiento esperado del servicio
-        when(noutTipprsService.findAllById(anyList())).thenReturn(tipoPrestaciones);
+		// Realiza la solicitud POST con los parámetros correctos
+		mockMvc.perform(MockMvcRequestBuilders.post("/regla-tipo-prestacion/save/1")
+							.flashAttr("regla", regla)
+							.param("tipoPrestacion", String.valueOf(tipoPrestaciones)))
+			.andExpect(MockMvcResultMatchers.status().is3xxRedirection())
+			.andExpect(MockMvcResultMatchers.view().name("redirect:/regla-tipo-prestacion/list"));
 
-        // Realiza la solicitud POST con los parámetros correctos
-        mockMvc.perform(MockMvcRequestBuilders.post("/regla-tipo-prestacion/save/1")
-                        .flashAttr("regla", regla)
-                        .param("tipoPrestacion", String.valueOf(tipoPrestaciones)))
-                .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
-                .andExpect(MockMvcResultMatchers.view().name("redirect:/regla-tipo-prestacion/list"));
-
-    }
+	}
 
 }
