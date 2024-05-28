5f22a6e4705c7f7fde591b562576dff239a3e040
Sergi Majada <118728080+smajada@users.noreply.github.com>
2024-05-28 12:25:13 +0200

    ref: added validations to ReglaDTO

--- sersoc-api/src/main/java/org/arteco/sersoc/dto/ReglaDTO.java
+++ sersoc-api/src/main/java/org/arteco/sersoc/dto/ReglaDTO.java
@@ -1,36 +1,40 @@
 package org.arteco.sersoc.dto;
 
+import java.util.Date;
+import java.util.List;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.arteco.sersoc.model.entities.NoutTipprs;
 import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.validation.constraints.NotEmpty;
-import java.util.Date;
-import java.util.List;
-
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class ReglaDTO {
 
-    private Long con;
+	private Long con;
 
-    @NotEmpty(message = "El identificador no puede ser vacío o nulo")
-    private String dec;
+	@NotNull(message = "El campo 'dec' no puede estar vacío")
+	@Size(min = 1, message = "El campo 'dec' debe tener al menos un carácter")
+	private String dec;
 
-    @DateTimeFormat(pattern = "yyyy-MM-dd")
-    private Date datIni;
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	@NotNull(message = "El campo 'datIni' no puede estar vacío")
+	private Date datIni;
 
-    @DateTimeFormat(pattern = "yyyy-MM-dd")
-    private Date datFin;
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	@NotNull(message = "El campo 'datFin' no puede estar vacío")
+	private Date datFin;
 
-    @NotEmpty(message = "El script no puede ser nulo o vacío")
-    private String script;
+	@NotNull(message = "El campo 'script' no puede estar vacío")
+	@Size(min = 1, message = "El campo 'script' debe tener al menos un carácter")
+	private String script;
 
-    private List<NoutTipprs> allTipoPrestacion;
+	private List<NoutTipprs> allTipoPrestacion;
 
-    private List<String> tipoPrestacionSelected;
+	private List<String> tipoPrestacionSelected;
 
 }
